1a6d57729be94f86cfc17b71d724b567383a4592
O Godswill Arinze <54944619+AZ259@users.noreply.github.com>
2022-12-21 03:30:57 -0800

    Update Web Design.js

--- asset/js/Web Design.js
+++ asset/js/Web Design.js
@@ -3,11 +3,11 @@
  let images = [
     "asset/media/images/Web Design/IMG_E4522.jpg",
     "asset/media/images/Web Design/IMG_E4526.jpg",
-    "asset/media/images/Web Design/IMG_E4523.jpg",
-    "asset/media/images/Web Design/IMG-20221213-WA0002.jpg",
-    "asset/media/images/Web Design/IMG-20221213-WA0014.jpg",
-    "asset/media/images/Web Design/IMG-20221213-WA0015.jpg",
-    "asset/media/images/Web Design/IMG-20221213-WA0019.jpg"
+   // "asset/media/images/Web Design/IMG_E4523.jpg",
+   // "asset/media/images/Web Design/IMG-20221213-WA0002.jpg",
+   // "asset/media/images/Web Design/IMG-20221213-WA0014.jpg",
+   // "asset/media/images/Web Design/IMG-20221213-WA0015.jpg",
+   // "asset/media/images/Web Design/IMG-20221213-WA0019.jpg"
  ];
  let time = 1000;  // changes every 1 seconds
 
